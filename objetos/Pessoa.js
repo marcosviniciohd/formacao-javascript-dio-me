86acf49237e92b210f5811aa6b5fd3feba3cc0cd
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-12-28 11:18:47 -0300

    Criando Instâncias com construtor

--- objetos/Pessoa.js
+++ objetos/Pessoa.js
@@ -1,14 +1,21 @@
 class Pessoa {
     nome;
     idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2023 - idade;
+    }
 
     descrever(){
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade} anos.`);
     }
 }
 
-const pessoa1 = new Pessoa();
-pessoa1.nome = 'Marcos Vinício';
-pessoa1.idade = 37;
+const pessoa1 = new Pessoa('Marcos Vinício', 37);
+const pessoa2 = new Pessoa('Daniela Oliveira', 28);
 
-console.log(pessoa1.descrever());
+console.log(pessoa1);
+console.log(pessoa2);
