c1715a45d954f6a053ae1ed8bac9a237e20f4b36
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-12-28 17:59:34 -0300

    Aula sobre arrays

---
+++ listas [arrays]/aula1.js
@@ -0,0 +1,10 @@
+const alunos = ['Marcos', 'Daniela', 'João', 'Maria', 'José'];
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+console.log(alunos[3]);
+console.log(alunos[4]);
+console.log(alunos[5]);
+alunos.push('Antônio');
+console.log(alunos[5]);
